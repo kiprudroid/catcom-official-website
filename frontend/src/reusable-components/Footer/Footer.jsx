73373beaa91b89ecb9726b7ade3ea1f8edd00bbf
kiprudroid <kiprutoshadrack06@gmail.com>
2025-02-15 17:55:59 +0300

    Created a Footer Component

--- frontend/src/reusable-components/Footer/Footer.jsx
+++ frontend/src/reusable-components/Footer/Footer.jsx
@@ -1,32 +1,38 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
-const Footer = () => {
+function Footer(){
     return (
-        <footer className="footer">
-            <div className="footer-content">
-                <div className="footer-grid-container">
-                    <div className="grid-item">
-                        <h3>Column 1</h3>
-                        <p>Content for the first column.</p>
-                    </div>
-                    <div className="grid-item">
-                        <h3>Column 2</h3>
-                        <p>Content for the second column.</p>
-                    </div>
-                    <div className="grid-item">
-                        <h3>Column 3</h3>
-                        <p>Content for the third column.</p>
-                    </div>
-                    <div className="grid-item">
-                        <h3>Column 4</h3>
-                        <p>Content for the fourth column, which is wider.</p>
-                    </div>
-                </div>
-               
+      <footer className="footer" style={{ fontFamily: "Roboto, sans-serif" }}>
+        <div className="footer-content">
+          <div className="footer-grid-container">
+            <div className="grid-item first">
+              <img
+                src="/others/ctm_logo.jpg"
+                alt="Catcom Logo"
+                className="ctm-logo"
+              />
+              <p className="location-wrapper">
+                JKUAT Catcom <br /> Main Campus <br />
+                Juja{" "}
+              </p>
             </div>
-        </footer>
+            <div className="grid-item">
+              <h3>Column 2</h3>
+              <p>Content for the second column.</p>
+            </div>
+            <div className="grid-item">
+              <h3>Column 3</h3>
+              <p>Content for the third column.</p>
+            </div>
+            <div className="grid-item">
+              <h3>Column 4</h3>
+              <p>Content for the fourth column, which is wider.</p>
+            </div>
+          </div>
+        </div>
+      </footer>
     );
-};
+}
 
 export default Footer;
