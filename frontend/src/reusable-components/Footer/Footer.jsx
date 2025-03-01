384f84231cbbfcbb2efb9d44e0f02bc6e036cb79
Ian Gitonga <iangitonga005@gmail.com>
2025-03-01 09:23:35 +0300

    removed an extra layer of div that enclosed the footer container

--- frontend/src/reusable-components/Footer/Footer.jsx
+++ frontend/src/reusable-components/Footer/Footer.jsx
@@ -20,63 +20,65 @@ function Footer({ className }) {
       className={`${styles.footer} ${className}`}
       style={{ fontFamily: "Roboto, sans-serif" }}
     >
-      <div className={styles.footerContent}>
-        <div className={styles.footerGridContainer}>
-          <div className={styles.gridItemFirst}>
-            <img
-              src="/others/ctm_logo.png"
-              alt="Catcom Logo"
-              className={styles.catcomLogo}
-            />
-            <p className={styles.locationWrapper}>
-              JKUAT Catcom <br /> Main Campus <br />
-              Juja
-            </p>
-          </div>
-          <div>
-            <FooterHeading>Quick Links</FooterHeading>
+      <div className={styles.footerContainer}>
+        <div className={styles.gridItemFirst}>
+          <img
+            src="/others/ctm_logo.png"
+            alt="Catcom Logo"
+            className={styles.catcomLogo}
+          />
+          <p className={styles.locationWrapper}>
+            JKUAT Catcom <br /> Main Campus <br />
+            Juja
+          </p>
+        </div>
+        <div>
+          <FooterHeading>Quick Links</FooterHeading>
+          {/* Quick Links column */}
+          <ul>
+            <li>Home</li>
+            <li>Liturgy</li>
+            <li>Community</li>
+            <li>Blog</li>
+            <li>Contacts</li>
+            <li>Register</li>
+          </ul>
+        </div>
 
-            <ul>
-              <li>Home</li>
-              <li>Liturgy</li>
-              <li>Community</li>
-              <li>Blog</li>
-              <li>Contacts</li>
-              <li>Register</li>
-            </ul>
-          </div>
-          <div className={styles.gridItem}>
-            <FooterHeading>Contact</FooterHeading>
-            <ul>
-              <li>
-                <FaFacebook />
-                &nbsp; &nbsp; catcomjkuat
-              </li>
-              <li>
-                <FaTiktok /> &nbsp; &nbsp; catcomjkuat
-              </li>
-              <li>
-                <FaInstagramSquare /> &nbsp; &nbsp; catcomjkuat{" "}
-              </li>
-              <li>
-                <FaTwitter /> &nbsp;&nbsp; catcomjkuat
-              </li>
-            </ul>
-          </div>
-          <div className={styles.gridItem}>
-            <h1>&nbsp;</h1>
-            <ul>
-              <li>
-                <FaYoutube /> &nbsp; &nbsp; catcomjkuat
-              </li>
-              <li>
-                <FaEnvelope /> &nbsp; &nbsp; jkuatcatcom18@gmail.com
-              </li>
-              <li>
-                <FaPhoneAlt /> &nbsp; &nbsp; +254 743 728621
-              </li>
-            </ul>
-          </div>
+        {/* contact column */}
+        <div>
+          <FooterHeading>Contact</FooterHeading>
+          <ul>
+            <li>
+              <FaFacebook />
+              &nbsp; &nbsp; catcomjkuat
+            </li>
+            <li>
+              <FaTiktok /> &nbsp; &nbsp; catcomjkuat
+            </li>
+            <li>
+              <FaInstagramSquare /> &nbsp; &nbsp; catcomjkuat{" "}
+            </li>
+            <li>
+              <FaTwitter /> &nbsp;&nbsp; catcomjkuat
+            </li>
+          </ul>
+        </div>
+
+        {/* contact column 2 */}
+        <div className={styles.gridItem}>
+          <h1>&nbsp;</h1>
+          <ul>
+            <li>
+              <FaYoutube /> &nbsp; &nbsp; catcomjkuat
+            </li>
+            <li>
+              <FaEnvelope /> &nbsp; &nbsp; jkuatcatcom18@gmail.com
+            </li>
+            <li>
+              <FaPhoneAlt /> &nbsp; &nbsp; +254 743 728621
+            </li>
+          </ul>
         </div>
       </div>
     </footer>
