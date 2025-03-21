f2b5cd75de59264528331b02b53806b25bc1b066
kili banks <wanmun3456@gmail.com>
2025-03-22 00:28:50 +0300

    created arrays of the community's executive leaders, patrons, priests and the groups in the community

---
+++ frontend/public/DataFiles/data.js
@@ -0,0 +1,140 @@
+const priests = [
+  {
+    name: "Prof(Fr.)Lawrence Njoroge",
+    role: "Chaplaincy",
+    image: "/chaplain/fatherLawrence.jpg",
+  },
+  {
+    name: "(Fr.)Patrick ",
+    role: "Parish priest",
+  },
+  {
+    name: "Prof(Fr.)Bernard",
+    role: "Assistant parish priest",
+    image: "",
+  },
+];
+
+const patrons = [
+  {
+    name: "professor Rebecca Waihenya",
+    role: "Patron",
+    image: "/patrons/profWaihenya.jpg",
+  },
+  {
+    name: "Mr. Alfred Wanzala",
+    role: "assistant patron",
+    image: "/patrons/wanzala.jpg",
+  },
+];
+
+const catcomExecutive = [
+  {
+    name: " Stephen Mutwiwa Mutie",
+    role: "Catcom moderator",
+    image: "/leadersImage/mutwiwa.jpg",
+  },
+  {
+    name: " Sophie Kathambi ",
+    role: "Catcom vice moderator",
+    image: "/leadersImage/mutwiwa.jpg",
+  },
+  {
+    name: " Stephen Mutwiwa Mutie",
+    role: "Catcom moderator",
+    image: "/leadersImage/mutwiwa.jpg",
+  },
+  {
+    name: " Eliza chiru",
+    role: "Catcom secretary",
+    image: "/leadersImage/eliza.jpg",
+  },
+  {
+    name: " terryann Wanjohi",
+    role: "Catcom treasurer",
+    image: "/leadersImage/terry.jpg",
+  },
+  {
+    name: " Alvin Muthoni",
+    role: "Catcom organizing secretary",
+    image: "/leadersImage/alvin.jpg",
+  },
+  {
+    name: " Bernard Nderitu",
+    role: "Catcom pastoral secretary",
+    image: "/leadersImage/nderitu.jpg",
+  },
+  {
+    name: " Albanus Timona",
+    role: "Catcom choir moderator",
+    image: "/leadersImage/engineer.jpg",
+  },
+  {
+    name: " Rodgers Matia",
+    role: "Catcom choir director",
+    image: "/leadersImage/matia.jpg",
+  },
+  {
+    name: " Robinson Koskei Cheruiyot",
+    role: "Catcom publicity secretary",
+    image: "/leadersImage/mutwiwa.jpg",
+  },
+  {
+    name: " Wilfred Wandera",
+    role: "Catcom technical secretary",
+    image: "/leadersImage/wandera.jpg",
+  },
+  {
+    name: " Sarah Okello",
+    role: "Catcom Catering secretary",
+    image: "/leadersImage/sarah okello.jpg",
+  },
+];
+
+const communities = [
+  {
+    title: "SCC",
+    content:
+      "A small Christian community is a smaller version of the entire CATCOM comprising of a smaller number of catcomers. They operate majorly within the campus. We have a total of eight SCCs",
+  },
+  {
+    title: "Choir",
+    content:
+      "We have a vibrant choir called KMRM (kwaya ya mtakatifu Romano Mtunzi) that keeps the community vibrant especially during mass",
+  },
+  {
+    title: "Liturgical dancers",
+    content:
+      "CATCOM liturgical dancers operate under the KMRM choir. they train and sing alongside the choir",
+  },
+  {
+    title: "Pastoral team",
+    content:
+      "the team handles the community's spiritual well being . they are responsible for mass preparation and following up on the community's progress in spiritual activities",
+  },
+  {
+    title: "Committees",
+    content:
+      "these are formed to help in organizing the community events such as hikes and retreats. these committees are formed and headed by the organizing secretary together with the publicity secretary ",
+  },
+  {
+    title: "Chatechism",
+    content:
+      "These are spiritual classes majorly spearheaded by the parish in line with the pastoral team to help members of the community get a chance to reeive the sacraments of the catholic church and also know their faith",
+  },
+  {
+    title: "Welfare Team",
+    content:
+      "This is a team led by the Catering secretary who is helped by the project managers and catering secretaries of all SCC groups. They are responsible for the well being of the community members in terms of financial, emotional support or any needs of the members",
+  },
+  {
+    title: "Technical Team",
+    content:
+      "the team is led by the technical secretary who is helped by the team in place. the teami s responsible for the community's technical equipment and the website of the community in terms of it's development and maintenance",
+  },
+  {
+    title: "Communion and Liberation and Bible prayer service",
+    content:
+      "this  are movements within the community that have external contact with other universities for common events under coordination from outside the university",
+  },
+];
