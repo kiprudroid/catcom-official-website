b93bb26a6baa91a3f80b667ddca5800b1864344c
kiprudroid <kiprutoshadrack06@gmail.com>
2025-03-22 04:02:12 +0300

    Made the Media Page responsive and added some placeholder images

--- frontend/src/pages/Community/Community.jsx
+++ frontend/src/pages/Community/Community.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import styles from "./Community.module.css";
 import Header from "../../reusable-components/Header/Header";
 import Footer from "../../reusable-components/Footer/Footer";
-import {
-  Heading,
-  Paragraph,
-  Text,
-} from "../../components/Typography/Typography";
+import { Heading } from "../../components/Typography/Typography";
 import SccCard from "../../components/SccCard/SccCard";
 import {SCCs} from "../../../public/DataFiles/data"
 
@@ -14,52 +10,55 @@ import {SCCs} from "../../../public/DataFiles/data"
 function Community() {
   return (
     <>
+     
       <div className={styles.gridContainer}>
         <div className={`${styles.item} ${styles.header}`}>
           <Header />
         </div>
-      </div>
-      <div className={`${styles.item} ${styles.sccPictures}`}>
-        <Heading>Our SCCs</Heading>
-        <div className={`contentWrapper ${styles.sccCardsWrapper}`}>
-          {SCCs.map((_, index) => (
-            <SccCard key={index} SccName={SCCs[index].name} />
-          ))}
-        </div>
         <div className={`${styles.item} ${styles.sccPictures}`}>
+          <Heading>Our SCCs</Heading>
           <div className={`contentWrapper ${styles.sccCardsWrapper}`}>
-            {[...Array(4)].map((index) => (
-              <SccCard key={index} />
+            {SCCs.map((_, index) => (
+              <SccCard key={index} SccName={SCCs[index].name} />
             ))}
           </div>
-        </div>
-        <div className={styles.sccItem}>
-          <div className={styles.secondColumnItem}>
-            <div className={styles.familyCard}>
-              <p>Family of Mary and Joseph</p>
+          
+          <Heading>SCC</Heading>
+          <div className={`${styles.item} ${styles.sccExpanded}`}>
+            <div className={styles.sccItem}>
+              <img
+                src="/others/group_photo.jpeg"
+                alt="SCC group Photo"
+                className={styles.groupPhoto}
+              />
             </div>
-            <div className={styles.familyCard}>
-              <p>Family of Mary and Joseph</p>
-            </div>
-            <div className={styles.familyCard}>
-              <p>Family of Mary and Joseph</p>
-            </div>
-            <div className={styles.familyCard}>
-              <p>Family of Mary and Joseph</p>
+            <div className={styles.sccItem}>
+              <div className={styles.secondColumnItem}>
+                <div className={styles.familyCard}>
+                  <p>Family of Mary and Joseph</p>
+                </div>
+                <div className={styles.familyCard}>
+                  <p>Family of Mary and Joseph</p>
+                </div>
+                <div className={styles.familyCard}>
+                  <p>Family of Mary and Joseph</p>
+                </div>
+                <div className={styles.familyCard}>
+                  <p>Family of Mary and Joseph</p>
+                </div>
+              </div>
+              <div className={styles.secondColumnItem}>Activities and Photos</div>
             </div>
           </div>
-          <div className={styles.secondColumnItem}>2nd</div>
-        </div>
-        <Heading>Alumni</Heading>
-        <div className={`${styles.item} ${styles.alumni}`}></div>
-        <Heading>Choir</Heading>
-
-        <div className={`${styles.item} ${styles.choir}`}></div>
-        <Heading>Technical Team</Heading>
-
-        <div className={`${styles.item} ${styles.technicalTeam}`}></div>
-        <div className={`${styles.item} ${styles.footer}`}>
-          <Footer />
+          <Heading>Alumni</Heading>
+          <div className={`${styles.item} ${styles.alumni}`}></div>
+          <Heading>Choir</Heading>
+          <div className={`${styles.item} ${styles.choir}`}></div>
+          <Heading>Technical Team</Heading>
+          <div className={`${styles.item} ${styles.technicalTeam}`}></div>
+          <div className={`${styles.item} ${styles.footer}`}>
+            <Footer />
+          </div>
         </div>
       </div>
     </>
