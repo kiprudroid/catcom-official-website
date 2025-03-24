219355a4dfe65b7338bc0c090c46ae6eda6b67ca
kiprudroid <kiprutoshadrack06@gmail.com>
2025-03-24 12:39:12 +0300

    Made the button fit the height of the card

--- frontend/src/components/communitiesCards/communityCard.jsx
+++ frontend/src/components/communitiesCards/communityCard.jsx
@@ -10,7 +10,7 @@ function CommunityCard({ title, content }) {
         <SmallText>{content}</SmallText>
       </div>
       <button className={styles.button}>
-        <SmallText>see more</SmallText>
+        <SmallText>See more</SmallText>
       </button>
     </div>
   );
