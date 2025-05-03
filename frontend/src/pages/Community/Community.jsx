439cb9a91d85327aaa207e6b21d4ed282bfeaabf
= <=>
2025-05-03 04:27:34 +0300

    Created the SCC expanded card for the Community Page

--- frontend/src/pages/Community/Community.jsx
+++ frontend/src/pages/Community/Community.jsx
@@ -1,12 +1,37 @@
 import React from "react";
+import { useState } from "react";
 import styles from "./Community.module.css";
 import Header from "../../reusable-components/Header/Header";
 import Footer from "../../reusable-components/Footer/Footer";
 import { Heading } from "../../components/Typography/Typography";
 import SccCard from "../../components/SccCard/SccCard";
 import { SCCs } from "../../DataFiles/data";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Community() {
+  const [selectedScc, setSelectedScc] = useState(SCCs[0]);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * SCCs.length);
+      setSelectedScc(SCCs[randomIndex]);
+    }, 20000);
+
+    return () => clearInterval(interval);
+  }, []);
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    adaptiveHeight: true,
+    fade: true,
+  };
   return (
     <>
       <div className={styles.gridContainer}>
@@ -26,29 +51,34 @@ function Community() {
             ))}
           </div>
 
-          <Heading>SCC</Heading>
+          <Heading>SCC : {selectedScc.name}</Heading>
           <div className={`${styles.item} ${styles.sccExpanded}`}>
             <div className={styles.sccItem}>
-              <img
-                src="/others/group_photo.jpeg"
-                alt="SCC group Photo"
-                className={styles.groupPhoto}
-              />
+              <div className={styles.slideShow}>
+                {selectedScc.sccPhotos && selectedScc.sccPhotos.length > 0 ? (
+                  <Slider {...settings} className={styles.sliderContainer}>
+                    {selectedScc.sccPhotos.map((image, index) => (
+                      <div key={index}>
+                        <img
+                          src={image}
+                          alt={`Slide ${index + 1}`}
+                          className={styles.slideshowImage}
+                        />
+                      </div>
+                    ))}
+                  </Slider>
+                ) : (
+                  <div className={styles.noImages}>No photos available</div>
+                )}
+              </div>
             </div>
             <div className={styles.sccItem}>
               <div className={styles.secondColumnItem}>
-                <div className={styles.familyCard}>
-                  <p>Family of Mary and Joseph</p>
-                </div>
-                <div className={styles.familyCard}>
-                  <p>Family of Mary and Joseph</p>
-                </div>
-                <div className={styles.familyCard}>
-                  <p>Family of Mary and Joseph</p>
-                </div>
-                <div className={styles.familyCard}>
-                  <p>Family of Mary and Joseph</p>
-                </div>
+                {selectedScc.families.map((family, index) => (
+                  <div className={styles.familyCard}>
+                    <p>{family}</p>
+                  </div>
+                ))}
               </div>
               <div className={styles.secondColumnItem}>
                 Activities and Photos
