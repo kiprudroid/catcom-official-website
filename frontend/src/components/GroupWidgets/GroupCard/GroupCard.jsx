ace26c0e82c8b429dde04579b644f5c45bc0e06e
eucabeth46 <bethawino2@gmail.com>
2025-09-05 11:23:51 +0300

    improve the group page

--- frontend/src/components/GroupWidgets/GroupCard/GroupCard.jsx
+++ frontend/src/components/GroupWidgets/GroupCard/GroupCard.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import styles from "./GroupCard.module.css";
 import { SectionHeading, Paragraph } from "../../Typography/Typography";
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const GroupCard = ({ data }) => {
+  const images = Array.isArray(data.imgSrc) ? data.imgSrc : [data.imgSrc];
+
   return (
     <div className={styles.groupCard}>
-      <img src={data.imgSrc} alt={data.alt} className={styles.cardImage} />
+        <div className={styles.imageSlideshow}>
+        <Carousel
+          showArrows={true}
+          showThumbs={false}
+          showStatus={false}
+          infiniteLoop
+          autoPlay
+          interval={3000}
+          className={styles.carousel}
+        >
+          {images.map((src, idx) => (
+            <div key={idx}>
+              <img src={src} alt={data.alt || `Group image ${idx + 1}`} className={styles.cardImage} />
+            </div>
+          ))}
+        </Carousel>
+      </div>
       <div className={styles.cardText}>
         <SectionHeading className={styles.cardTitle}>
           {data.title}
